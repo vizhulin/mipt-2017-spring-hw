68bd61a4e51ae739290ac822d377ff01fc27308b
Mikhaylovich Dmitry <dmitry.mikhaylovich@bostongene.com>
2017-03-07 19:47:19 +0300

    задание BigInt

---
+++ src/cpp/lesson5/BigInt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dmitry on 3/7/17.
+//
+
+#include "BigInt.h"
---
+++ src/cpp/lesson5/BigInt.h
@@ -0,0 +1,14 @@
+//
+// Created by dmitry on 3/7/17.
+//
+
+#ifndef MIPT_2017_SPRING_HW_BIGINT_H
+#define MIPT_2017_SPRING_HW_BIGINT_H
+
+
+class BigInt {
+    //предоставляю Вам свободу
+};
+
+
+#endif //MIPT_2017_SPRING_HW_BIGINT_H
